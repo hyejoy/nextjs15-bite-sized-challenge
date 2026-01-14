d31205e09bb16c1ee8c7c7e9297322b04c23ea67
hyejoy <devchoi97@gmail.com>
2026-01-14 21:51:37 +0900

    feat(section04): 레이아웃에 검색바 클라이언트 컴포넌트 적용
    [Next.js] 04-04) 리액트 서버 컴포넌트 이해하기
    
    - 학습 내용:
      - App Router 환경에서 layout 컴포넌트 구성 실습
      - 서버 컴포넌트(layout)와 클라이언트 컴포넌트(SearchBar) 분리 구현
      - "use client" 지시자를 통한 상태 기반 입력 처리 실습
    
    - 핵심 포인트:
      - layout은 기본적으로 서버 컴포넌트로 동작한다
      - 클라이언트 컴포넌트는 필요한 경우에만 분리해 사용한다
      - 서버 컴포넌트 트리 하위에 클라이언트 컴포넌트를 포함할 수 있다

--- 04-app-router/section04/src/app/(with-searchbar)/layout.tsx
+++ 04-app-router/section04/src/app/(with-searchbar)/layout.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from "react";
+import SearchBar from "./searchbar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <h1>임시 서치바</h1>
+      <SearchBar />
       {children}
     </>
   );
